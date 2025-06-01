f0f9e97e117a2d66c5977ff47bdac2a01542efa9
nyanters <123231885+nyanters@users.noreply.github.com>
2025-06-01 20:14:01 +0900

    modified:   qemu-virgl.rb

--- Formula/qemu-virgl.rb
+++ Formula/qemu-virgl.rb
@@ -13,7 +13,7 @@ class QemuVirgl < Formula
   depends_on "glib"
   depends_on "gnutls"
   depends_on "jpeg"
-  depends_on "nyanters/qemu-virgl/libangle"
+  depends_on "knazarov/qemu-virgl/libangle"
   depends_on "nyanters/qemu-virgl/libepoxy-angle"
   depends_on "nyanters/qemu-virgl/virglrenderer"
   depends_on "libpng"
